e2639ad5f1a2c144881793550dd2e67389953de4
Kenneth Chen <137091914+kennethchen416@users.noreply.github.com>
2025-08-06 12:38:32 -0400

    security

--- dummy.js
+++
@@ -1,4 +0,0 @@
-// dummy.js - DO NOT COMMIT THIS FILE WITH REAL KEYS TO A PUBLIC REPO
-// For local testing, you can fill these in. For GitHub Pages, use GitHub Secrets (see step 4).
-window.SUPABASE_URL = 'https://ggxvzeqgpnbjaibbzign.supabase.co'; // Paste your Project URL here
-window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdneHZ6ZXFncG5iamFpYmJ6aWduIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ0NTA1NzgsImV4cCI6MjA3MDAyNjU3OH0.RBxrY_JMTzsBf9qIrIsBceJxgH6KR6XP12p5442_4wo'; // Paste your anon public key here
